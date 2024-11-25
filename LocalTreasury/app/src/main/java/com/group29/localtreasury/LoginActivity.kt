41454b38d1b086f06063e66bf011ea23671ef003
Birfatehjit Grewal <Birfateh12@gmail.com>
2024-11-24 23:42:07 -0800

    login activity comment issue fix in onstart

--- LocalTreasury/app/src/main/java/com/group29/localtreasury/LoginActivity.kt
+++ LocalTreasury/app/src/main/java/com/group29/localtreasury/LoginActivity.kt
@@ -55,7 +55,6 @@ class LoginActivity : AppCompatActivity() {
 
     public override fun onStart() {
         super.onStart()
-        /**
         // Check if user is signed in (non-null) and update UI accordingly.
 //        val currentUser = auth.currentUser
 //        if (currentUser != null) {
